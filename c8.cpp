1bde7e739d5f7634c8b64520dff610c4081857af
artv <vaz985@gmail.com>
2018-08-07 22:26:49 -0300

    Init Emu

---
+++ c8.cpp
@@ -0,0 +1,76 @@
+#include "c8.h"
+
+void chip8::insert_rom() {
+	std::string rom_dir;
+	std::ifstream rom("pong2.c8", std::ifstream::binary);
+	//std::cout << "Input rom dir: " << std::endl;
+	//while(rom.good()) {
+	//	std::cout << "try" << std::endl;
+	//	std::cin >> rom_dir;
+	//	rom.open(rom_dir, std::ifstream::binary);	
+	//}
+	rom.seekg(0, std::ios_base::end);
+	int lenght = rom.tellg();
+	rom.seekg(0, std::ios_base::beg);
+
+	char * buffer = new char [lenght];
+	rom.read(buffer, lenght);
+	for(int i=0; i<80; i++) {
+		this->memory[512 + i] = buffer[i];
+	}
+}
+void chip8::initialize() {
+	insert_rom();
+
+	// Init RNG
+	this->rng.seed(std::random_device()());
+
+	// Clean
+	this->pc		 = 0x200; // 
+	this->opcode = 0;			//
+	this->I			 = 0;			//
+	this->sp		 = 0;			//
+	
+	for(int i=0; i<80; i++) {
+		this->memory[i] = chip8_fontset[i];
+	}
+	// reset timers
+}
+
+
+void chip8::emulateCycle() {
+	// Fetch
+	this->opcode = this->memory[pc] << 8 | this->memory[pc+1];
+	// Decode & Execute
+	switch(this->opcode & 0xF000) {
+
+		// 0NNN: Calls RCA 1802 program at address NNN. Not necessary for 
+		// most ROMs. 
+		case 0x0000:
+			std::cout << "0NNN Not defined" << std:: endl;	
+		break;
+
+
+		// ANNN: Sets I to the address NNN
+		case 0xA000: 			
+			this->I = this->opcode & 0x0FFF;
+			this->pc += 2;
+		break;
+
+		// BNNN: Jumps to the address NNN plus V0.
+		case 0xB000: 
+			this->pc = this->V[0] + (this->opcode & 0x0FFF);
+		break;
+
+		// CNNN: Sets VX to the result of a bitwise and operation on a random 
+		// number (Typically: 0 to 255) and NN. 
+		case 0xC000:
+			std::uniform_int_distribution<std::mt19937::result_type> dist(0,15);
+			unsigned char X = this->opcode & 0x0F00;
+			unsigned char rand = dist(this->rng);
+		break;
+
+	}
+
+	// Update timers
+}
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "c8.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+	chip8 c8;
+	c8.initialize();
+
+	return 0;
+}
